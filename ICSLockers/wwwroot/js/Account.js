1243b974790b1bf683bda502db5f1adbc6caa9d0
Manimaran <32846534+Anilkumar18@users.noreply.github.com>
2023-03-28 21:56:52 +0530

    Validate Usercreation page.

--- ICSLockers/wwwroot/js/Account.js
+++ ICSLockers/wwwroot/js/Account.js
@@ -4,7 +4,7 @@
 });
 
 
-function fnLoginMethod () {
+function fnLoginMethod() {
     var password = $(".user-passcode").val().trim();
 
     if (password == null || password == "") {
@@ -30,7 +30,7 @@ function fnLoginMethod () {
     });
 }
 
-function fnLogoutMethod () {
+function fnLogoutMethod() {
     $.ajax({
         type: "POST",
         url: "/Account/Logout",
@@ -44,55 +44,93 @@ function fnLogoutMethod () {
     });
 }
 
-function fnNewUserCreation () {
+function fnNewUserCreation() {
     debugger;
-
-    //var userCreationModel = {
-    //    UserName: $(".create-user-form .first-name").val().trim() + $(".create-user-form .last-name").val().trim(),
-    //    Email: $(".create-user-form .email-id").val(),
-    //    PasswordHash: "Admin",
-    //    FirstName: $(".create-user-form .first-name").val(),
-    //    LastName: $(".create-user-form .last-name").val(),
-    //    SSN: $(".create-user-form .ssn").val(),
-    //    LockerUnit: $(".create-user-form .locker-unit").val(),
-    //    PhoneNumber: $(".create-user-form .mobile-no").val(),
-    //    CheckOutStatus: false,
-    //    /*CreatedOn: n,*/
-    //    CreatedBy: "Admin",
-    //    //NormalizedUserName: null,
-    //    //NormalizedEmail: null,
-    //    //EmailConfirmed: false,
-    //    //SecurityStamp: null,
-    //    //ConcurrencyStamp: null,
-    //}
 
     var userCreationModel = {
         UserName: $(".create-user-form .first-name").val().trim() + $(".create-user-form .last-name").val().trim(),
         Email: $(".create-user-form .email-id").val(),
         FirstName: $(".create-user-form .first-name").val().trim(),
         LastName: $(".create-user-form .last-name").val().trim(),
-        SSN: $(".create-user-form .ssn").val(),
+        SSN: parseInt($(".create-user-form .ssn").val()),
         PasswordEnc: "password123",
-        LockerUnit: $(".create-user-form .locker-unit").val(),
+        LockerUnit: $(".create-user-form .locker-unit").val() ,
         CreatedOn: new Date(),
         CreatedBy: "Admin",
-        CheckOutStatus: false
+        CheckOutStatus: false,
+        PhoneNumber: parseInt($(".create-user-form .mobile-no").val())
     };
 
-    $.ajax({
-        type: "POST",
-        url: "/api/user",
-      /*  async: true,*/
-        headers: { "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val() },
-        /*contentType: 'application/json; charset=utf-8',*/
-        contentType: "application/json",
-        data: JSON.stringify(userCreationModel),
-        success: function (data) {
-            window.location.href = "/";
-        },
-        error: function (xhr, status, error) {
-            // Handle errors here
-            console.log(error);
-        }
-    });
+    var isValidated = ValidateUserCreation(JSON.stringify(userCreationModel));
+
+
+    if (isValidated) {
+
+        $.ajax({
+            type: "POST",
+            url: "/api/user",
+            headers: { "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val() },
+            contentType: "application/json",
+            data: JSON.stringify(userCreationModel),
+            success: function (data) {
+                window.location.href = "/";
+            },
+            error: function (xhr, status, error) {
+                console.log(error);
+            }
+        });
+    }
+}
+
+function ValidateUserCreation(userCreationJSONModel) {
+    var userCreationModel = JSON.parse(userCreationJSONModel);
+
+    // Validate first name is not empty
+    if (!userCreationModel.FirstName) {
+        $(".first-name-div").addClass("form-error");
+        $(".first-name-div #error").text("Enter the first name");
+    }
+
+    // Validate last name is not empty
+    if (!userCreationModel.LastName) {
+        $(".last-name-div").addClass("form-error");
+        $(".last-name-div #error").text("Enter the last name");
+    }
+    //Validate Email is not empty
+    if (!userCreationModel.Email) {
+        $(".email-div").addClass("form-error");
+        $(".email-div #error").text("Enter the correct email!");
+    }
+
+    if (typeof userCreationModel.PhoneNumber != "number" || userCreationModel.PhoneNumber.toString().length != 10) {
+        $(".mobile-div").addClass("form-error");
+        $(".mobile-div #error").text("Enter the correct Mobile number!");
+    }
+    // Validate SSN format
+
+    if (typeof userCreationModel.SSN != "number" || userCreationModel.SSN.toString().length!=9) {
+        $(".ssn-div").addClass("form-error");
+        $(".ssn-div #error").text("Enter the ssn 9 digit number");
+    }
+
+    // All fields are valid
+    return $(".form-error").length > 0 ? false : true;
+
+}
+
+function fnValidateEmail(ctrl) {
+    debugger;
+
+    var currentValue = $(ctrl).val();
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(currentValue)) {
+        $(ctrl.parentElement).addClass("form-error");
+        $(ctrl.nextElementSibling).text("Please fill the correct email!");
+    }
+}
+
+function fnRemoveValidation(ctrl) {
+    $(ctrl.parentElement).removeClass("form-error");
+    $(ctrl.nextElementSibling).text("")
 }

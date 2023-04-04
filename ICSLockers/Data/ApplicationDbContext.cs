83e48e2bc6b77b326c2ee71b5279c78702e5b901
Elanchezhiyan-P <elanchezhiyan.p@aitechindia.com>
2023-04-04 13:08:19 +0530

    Updated seeded users and their roles.

--- ICSLockers/Data/ApplicationDbContext.cs
+++ ICSLockers/Data/ApplicationDbContext.cs
@@ -15,7 +15,37 @@ namespace ICSLockers.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            SeedUsers(builder);
             SeedRoles(builder);
+            SeedUserRoles(builder);
+        }
+
+        private void SeedUsers(ModelBuilder builder)
+        {
+            ApplicationUser user = new()
+            {
+                Id = "b74ddd14-6340-4840-95c2-db12554843e5",
+                UserName = "elanchezhiyan.p@aitechindia.com",
+                NormalizedUserName = "elanchezhiyan.p@aitechindia.com",
+                Email = "elanchezhiyan.p@aitechindia.com",
+                NormalizedEmail = "elanchezhiyan.p@aitechindia.com",
+                LockoutEnabled = true,
+                PhoneNumber = "9942644999",
+                EmailConfirmed = true,
+                FirstName = "Elanchezhiyan",
+                LastName = "P",
+                CreatedOn = DateTime.Now,
+                CreatedBy = "Admin",
+                SSN = 987654311,
+                CheckOutStatus = false,
+                PasswordEnc = "EP11",
+                LockerUnit = 0,                
+            };
+
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHasher.HashPassword(user, user.PasswordEnc);
+
+            builder.Entity<ApplicationUser>().HasData(user);
         }
 
         private static void SeedRoles(ModelBuilder builder)
@@ -25,6 +55,13 @@ namespace ICSLockers.Data
                 new IdentityRole { Id= "c76d8f8d-9e53-433c-b8e3-96fddb7ac25b", Name = "Staff", ConcurrencyStamp = "2", NormalizedName = "Staff" },
                 new IdentityRole { Id= "865d7c89-436d-43a3-946b-f36896d64ccf", Name = "User", ConcurrencyStamp = "3", NormalizedName = "User" }
             );
+        }
+
+        private static void SeedUserRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>() { RoleId = "b6011125-2b8d-442a-9717-b8cf5345b015", UserId = "b74ddd14-6340-4840-95c2-db12554843e5" }
+                );
         }
     }
 }
--- ICSLockers/Data/Migrations/20230404071658_UserRoles.Designer.cs
+++ ICSLockers/Data/Migrations/20230404071658_UserRoles.Designer.cs
@@ -4,6 +4,7 @@ using ICSLockers.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ICSLockers.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230404071658_UserRoles")]
+    partial class UserRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

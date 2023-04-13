ea8c656be4f7f6b8f0f89fd3a55b811fea0b3bf3
Elanchezhiyan-P <elanchezhiyan.p@aitechindia.com>
2023-04-13 21:44:21 +0530

    Updated dashboard model.

---
+++ ICSLockers/Models/AdminDashboard.cs
@@ -0,0 +1,11 @@
+﻿namespace ICSLockers.Models
+{
+    public class AdminDashboard
+    {
+        public int TotalLocation { get; set; }
+        public int TotalDivisions { get; set; }
+        public int TotalLockers { get; set; }
+        public int LockerUsed { get; set; }
+        public List<ApplicationUser>? UserList { get; set; }
+    }
+}

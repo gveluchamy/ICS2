e351f97f787efdea5ecd9a642aaf528e9a69593c
Poovarasan3011 <poovarasan.ait@gmail.com>
2023-04-25 20:46:22 +0530

    Design changes

--- ICSLockers/wwwroot/js/Staff.js
+++ ICSLockers/wwwroot/js/Staff.js
@@ -9,3 +9,15 @@
 function fnRemoveUserPopup() {
     $("#RemoveUserModal").modal("show");
 }
+
+var $el = $("#options img");
+var $ee = $("#options #menus");
+$el.click(function (e)  
+    e.stopPropagation();    
+    $("#options #menus").toggleClass('show');
+});
+$(document).on('click', function (e) {
+    if (($(e.target) != $el) && ($ee.hasClass('show'))) {
+        $ee.removeClass('show');
+    }
+});
